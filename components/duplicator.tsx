88c51602c29199ac39e44913e5dcda13d219faea
Joe Rall <joseph.rall@live.com>
2021-06-25 09:07:45 -0400

    Simplified project

--- components/duplicator.tsx
+++ components/duplicator.tsx
@@ -10,15 +10,7 @@ const Duplicator: React.FC<DuplicatorProps> = (props) => {
   return (
     <div>
       <h1>Hello {props.id}</h1>
-      <div
-        style={{
-          display: "flex",
-          flex: "row",
-        }}
-      >
-        <NextImage src={quarry} width={100} height={100} />
-        {/* <img {...(quarry as any)} width={100} height={100} /> */}
-      </div>
+      <NextImage src={quarry} width={100} height={100} />
     </div>
   );
 };
